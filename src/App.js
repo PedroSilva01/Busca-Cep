7a972f26a5e18eff4074ff9c0b0e181b212d5461
Pedro Silva <pedrovictor94442500@gmail.com>
2022-07-13 20:22:42 -0300

    interface

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
+import { FiSearch }  from 'react-icons/fi'
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Buscador de Cep</h1>
+    <div className="container">
+      <h1 className="title">Buscador de Cep</h1>
+      <div className='containerInput'>
+        <input 
+          placeholder="Digite o seu cep"
+          type="text"
+        />
+        <button className="buttonSearch"><FiSearch size={25} color="#FFF"/></button>
+      </div>      
+      <main className='main'>
+        <h2>CEP: 873487643</h2>
+        <span>Rua:</span>
+        <span>Complemento:</span>
+        <span>Bairro:</span>
+        <span>Localidade: </span>
+      </main>
     </div>
+
+
   );
 }
 
