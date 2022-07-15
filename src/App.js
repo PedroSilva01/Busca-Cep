ad536002037b9bf9b8a46470da8fb0897ea7bc38
Pedro Silva <pedrovictor94442500@gmail.com>
2022-07-15 10:24:25 -0300

    finalized

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import { useState } from 'react';
 import { FiSearch }  from 'react-icons/fi';
 import './style.css';
+import api from './services/api'
 
 function App() {
 
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
 
-  function handleSearch(){
+  async function handleSearch(){
     //fazendo uma requisição http
     
     if (input === ''){
       alert('Preencha o campo corretamente')
       return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput('') 
+      if(response.data.logradouro == ''){
+        cep.logradouro = "não há logradouro registrado"
+      }
+    }catch{
+      alert('Ops.. erro ao buscar')
+      setInput('') //volta o  valor do useState pra vazio
     }
   }
 
@@ -23,16 +37,21 @@ function App() {
           placeholder="Digite o seu cep"
           type="text"
           value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
         <button className="buttonSearch" onClick={handleSearch}><FiSearch size={25} color="#FFF"/></button>
-      </div>      
-      <main className='main'>
-        <h2>CEP: 873487643</h2>
-        <span>Rua:</span>
-        <span>Complemento:</span>
-        <span>Bairro:</span>
-        <span>Localidade: </span>
-      </main>
+      </div>
+      {/*a partir da linha de baixo ele ta fazendo uma renderização condicional, só vai aparecer o campo de resposta se o cep for maior que 0, por isso utilizou o "&&" pois se aquilo for verdadeiro, vai renderizar o que tiver dentro dos parenteses a seguir */}
+      {Object.keys(cep).length > 0 && (
+        <main className='main'> 
+          <h2>CEP: {cep.cep}</h2>
+          <span>Rua: {cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>Bairro: {cep.bairro}</span>
+          <span>Localidade: {cep.localidade} - {cep.uf}</span>
+        </main>
+      )}      
+
     </div>
 
 
