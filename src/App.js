2fa73071904df7ad0d8039cc6c918f2cb2aa6e01
Pedro Silva <pedrovictor94442500@gmail.com>
2022-07-15 09:41:29 -0300

    req http

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import { FiSearch }  from 'react-icons/fi'
+import { useState } from 'react';
+import { FiSearch }  from 'react-icons/fi';
 import './style.css';
 
 function App() {
+
+  const [input, setInput] = useState('')
+
+  function handleSearch(){
+    //fazendo uma requisição http
+    
+    if (input === ''){
+      alert('Preencha o campo corretamente')
+      return;
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador de Cep</h1>
@@ -9,8 +22,9 @@ function App() {
         <input 
           placeholder="Digite o seu cep"
           type="text"
+          value={input}
         />
-        <button className="buttonSearch"><FiSearch size={25} color="#FFF"/></button>
+        <button className="buttonSearch" onClick={handleSearch}><FiSearch size={25} color="#FFF"/></button>
       </div>      
       <main className='main'>
         <h2>CEP: 873487643</h2>
